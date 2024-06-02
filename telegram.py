d48f8e8189fbd19a8a549151a8b5270957a64dad
Dmytro Ruzhyckyi <58374128+dimxn@users.noreply.github.com>
2024-06-02 14:06:21 +0000

    add some features

--- telegram.py
+++ telegram.py
@@ -1,2 +1,4 @@
 import telebot
 bot = telebot.TeleBot('6366506609:AAHcDRADOVP2EbFUy6oBV8p80vhvew6bC7Q')
+API_URL = "https://665c4b543e4ac90a04d9299c.mockapi.io/schedule/"
+school_url = "https://goncharivkaschool.e-schools.info/m/news"
