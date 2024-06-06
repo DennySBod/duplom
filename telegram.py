5dd031ecb1fb228114f8b8ffcd3a7ed4567029cf
DennySBod <denchuk86@gmail.com>
2024-06-06 11:48:58 +0300

    smail

--- telegram.py
+++ telegram.py
@@ -1,5 +1,5 @@
 import telebot
-bot = telebot.TeleBot('6366506609:AAHcDRADOVP2EbFUy6oBV8p80vhvew6bC7Q')
+bot = telebot.TeleBot('7342010371:AAG_ytS9-Q9lCwMa47zWbJBYq5-i0LOQrGE')
 API_URL = "https://665c4b543e4ac90a04d9299c.mockapi.io/schedule/"
 school_url = "https://goncharivkaschool.e-schools.info/m/news"
 weather_url = "https://ua.sinoptik.ua/погода-гончарівка-303005669/"
